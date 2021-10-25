0e6e705212eb39a2506ec298d46eb808d74dc77b
davidferpe99 <60969608+davidferpe99@users.noreply.github.com>
2021-10-25 10:25:55 +0200

    Create Ejercicio2.cc

---
+++ Práctica 2.1/Ejercicio2.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+int main (){
+    char *c;
+    if (setuid(0) == -1){
+        printf("ERROR(%d): %s\n", errno, strerror(errno));
+    }
+    
+    return 1;
+}
