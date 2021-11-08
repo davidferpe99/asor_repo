447884e3c0d47182b653411666671865f2c7dbfd
davidferpe99 <60969608+davidferpe99@users.noreply.github.com>
2021-11-08 10:18:46 +0100

    Create Ejercicio7.cc

---
+++ Práctica 2.2/Ejercicio7.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(){
+
+    
+
+    mode_t mask = umask(027);
+
+    int file = open("fichero2.txt", O_CREAT | O_RDONLY, 0777);
+
+
+
+    return 1;
+}
